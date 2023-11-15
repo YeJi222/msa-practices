9277c177d6c5d9f555008275119b61f6e806199c
YeJi222 <hhongyizi@gmail.com>
2023-11-15 13:20:45 +0900

    feat: discovery client & mysite07

---
+++ clients/emaillist08/backend/src/main/resources/script/launch.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# export PATH=/usr/local/poscodx2023/java/bin
+
+APPLICATION_NAME=emaillist07 # jar 이름으로 맞춰주기
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+PID=$(ps -ef | grep java | grep $APPLICATION_NAME.jar | awk '{print $2}')
+
+if  [ ! -z "$PID" ] 
+then
+	echo "stopping [$APPLICATION_NAME]"
+	kill -9 $PID
+	sleep 10
+fi
+
+echo "starting [$APPLICATION_NAME]"
+cd $SCRIPT_DIR
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar >> $SCRIPT_DIR/launch.log &
+# nohup /usr/local/poscodx2023/java/bin/java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar >> $SCRIPT_DIR/launch.log &
